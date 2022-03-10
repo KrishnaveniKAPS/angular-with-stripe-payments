6677aece487fbab73c6831552f5aaa4da80790f8
“KrishnaveniKAPS”@gmail.com” <kkrishnaveni644@gmail.com>
2022-03-10 11:59:18 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-with-stripe-payments';
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+
+  constructor() { }
+  handler: any = null;
+  ngOnInit() {
+
+    this.loadStripe();
+  }
+
+  pay(amount: any) {
+
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51KYQgBSFEoL0A5fDJxlpuB95uz2DJw8foELYyDH3JiFz78usi3mrCLfrlHdiy7HYYJscxxK4OzklvcYDpjyYO0MZ00Fg4dq9Uu',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token)
+        alert('Token Created!!');
+      }
+    });
+
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: amount * 100
+    });
+
+  }
+
+  loadStripe() {
+
+    if (!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id = "stripe-script";
+      s.type = "text/javascript";
+      s.src = "https://checkout.stripe.com/checkout.js";
+      s.onload = () => {
+        this.handler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51KYQgBSFEoL0A5fDJxlpuB95uz2DJw8foELYyDH3JiFz78usi3mrCLfrlHdiy7HYYJscxxK4OzklvcYDpjyYO0MZ00Fg4dq9Uu',
+          locale: 'auto',
+          token: function (token: any) {
+            // You can access the token ID with `token.id`.
+            // Get the token ID to your server-side code for use.
+            console.log(token)
+            alert('Payment Success!!');
+          }
+        });
+      }
+
+      window.document.body.appendChild(s);
+    }
+  }
 }
